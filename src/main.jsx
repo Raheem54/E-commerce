ccd8ff148d6ee189bc75e25ca5c2f910c079500c
Raheem Elnashar <fastfast415@gmail.com>
2025-11-29 06:24:25 +0200

    Update icon of website

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import TokenContextProvider from '../src/context/Tocken.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import CartContextProvider from './context/cartContext.jsx'
+import CartContextProvider from './context/CartContext.jsx'
 let qurey = new QueryClient()
 createRoot(document.getElementById('root')).render(
     <CartContextProvider>
